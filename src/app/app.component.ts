36d67b29c7ba372673d3427c3a1d3539f4115aae
Alexey <asdalexey@yandex.ru>
2018-06-13 23:38:55 +0300

    add basic source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+// import 'rxjs/add/observable/map';
+// import 'rxjs/add/operator/tap';
+// import 'rxjs/add/operator/do';
+// import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  subject$ = new BehaviorSubject({ data: [{ id: 10, name: 'Alexey' }] });
+
+  constructor() {
+    // this.subject$
+    //     .do(console.log)
+    //     .map(results => results.data)
+    //     .subscribe(results => {
+    //       console.log('Results', results);
+    //     });
+
+    this.subject$.pipe(
+      tap(console.log),
+      map(results => results.data[0])
+    ).subscribe(results => {
+      console.log('Results', results);
+    });
+  }
 }
